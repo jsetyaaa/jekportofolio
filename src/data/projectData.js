14c2694c526ad9355845659a490ad4ad9d330227
jsetyaaa <jsetyaaa@gmail.com>
2025-05-19 18:58:10 +0700

    Update DetailPage, add some projects

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,3 +1,4 @@
+import { desc } from "framer-motion/client";
 import images from "../assets/images";
 
 const projectData = [
@@ -5,6 +6,18 @@ const projectData = [
     title: "Landing Page PlastEco Solutions",
     description:
       "PlastEco Solutions menyadari bahwa tanpa tindakan yang tepat, limbah plastik akan terus mengancam keberlanjutan lingkungan dan kesehatan manusia. Oleh karena itu, kami menawarkan solusi lengkap untuk pengelolaan limbah plastik dengan pendekatan yang holistik dan berkelanjutan. Dengan layanan pengumpulan, pemrosesan, dan daur ulang yang efisien, kami membantu mengurangi jumlah limbah plastik yang berakhir di lautan dan lingkungan sekitarnya.",
+    fitur: "Pencarian, Pengolahan, dan Ekstraksi Data",
+    teams: [
+      {
+        name: "Yudi",
+        role: "Frontend",
+      },
+      {
+        name: "Bagas",
+        role: "Backend",
+      },
+    ],
+    tech: "ReactJS, NodeJS, ExpressJS, TailwindCSS",
     thumbnail: images["Plasteco.webp"],
     url: "https://plasteco-solutions.vercel.app/",
     slug: "plasteco-solutions",
@@ -12,6 +25,40 @@ const projectData = [
   {
     title: "Srabeling Apps",
     description: "Srabeling Apps merupakan aplikasi web yang dibuat menggunakan Flask, yang secara khusus dirancang untuk membantu peneliti dalam mengekstrak data dari Google Play Store. Srabeling Apps memberikan kemudahan dan efisiensi bagi peneliti yang perlu mengakses data aplikasi untuk analisis lebih lanjut, sehingga menjadikannya alat yang sangat berharga untuk penelitian aplikasi seluler.",
+    fitur: [
+      {
+        name: "Scraping Data",
+        description: "Aplikasi Dapat Melakukan Scraping Data Dari Google Play Store",
+      },
+      {
+        name: "Filter Data",
+        description: "Aplikasi Dapat Melakukan Filter Data Dari Google Play Store Berdasarkan Stars",
+      },
+      {
+        name: "Labeling Data",
+        description: "Aplikasi Dapat Melakukan Labeling Data menggunakan Model Support Vector Machine",
+      },
+      {
+        name: "Visualisasi Data",
+        description: "Aplikasi Dapat Melakukan Visualisasi Data yang telah di Scraping",
+      },
+      {
+        name: "Download Data",
+        description: "Aplikasi Dapat Melakukan Download Data yang telah di Scraping",
+      },
+    ],
+    teams: [
+      {
+        name: "Solo Projects",
+        role: "Fullstack",
+      }
+    ],
+    tech: [
+      "Python",
+      "Flask",
+      "HTML",
+      "TailwindCSS",
+    ],
     thumbnail: images["Srabeling.webp"],
     url: null,
     slug: "srabeling",
@@ -20,6 +67,33 @@ const projectData = [
     title: "Blog TechnoAI",
     description:
       "Techno AI adalah web blog informatif seputar teknologi dan kecerdasan buatan, dibangun menggunakan PHP Laravel. Menyajikan artikel, berita, dan tutorial terkini, Techno AI hadir sebagai sumber terpercaya bagi para pengembang, pelajar, dan pecinta teknologi yang ingin mengikuti perkembangan dunia AI secara mendalam dan berkelanjutan.",
+    fitur: [
+      {
+        name: "Blog",
+        description: "Menampilkan artikel, berita, dan tutorial terkini tentang teknologi dan kecerdasan buatan.",
+      },
+      {
+        name: "Dashboard CMS",
+        description: "Melakukan pengelolaan artikel, kategori artikel, berita, dan tutorial.",
+      },
+      {
+        name: "SEO",
+        description: "Memudahkan pencarian halaman web melalui mesin pencari.",
+      },
+    ],
+    teams: [
+      {
+        name: "Solo Projects",
+        role: "Fullstack",
+      }
+    ],
+    tech: [
+      "PHP 8.7.3",
+      "Laravel 11.0.2",
+      "Sweetalert 2.1.2",
+      "HTML",
+      "TailwindCSS",
+    ],
     thumbnail: images["Technoai.webp"],
     url: null,
     slug: "technoai",
@@ -27,9 +101,89 @@ const projectData = [
   {
     title: "Landing Page Arjabina",
     description: "Arjabina adalah penyedia jasa desain arsitek, interior, dan kontraktor Jogja yang profesional dan terpercaya. Dengan pengalaman dan keahlian yang kuat dalam industri desain, kami menyediakan solusi desain yang sesuai dengan kebutuhan dan keinginan anda.",
+    fitur: [
+      {
+        name: "Landing Page",
+        description: "Landing Page yang menampilkan informasi tentang Arjabina dan layanan yang disediakan.",
+      },
+      {
+        name: "Call To Action",
+        description: "Call To Action yang memudahkan pengunjung untuk menghubungi Arjabina.",
+      },
+      {
+        name: "SEO",
+        description: "Memudahkan pencarian halaman web melalui mesin pencari.",
+      }
+    ],
+    teams: [
+      {
+        name: "Solo Projects",
+        role: "UI Designer and Front End",
+      }
+    ],
+    tech: [
+      "ReactJS",
+      "TailwindCSS",
+    ],
     thumbnail: images["Arjabina.webp"],
     url: null,
     slug: "arjabina",
+  },
+  {
+    title: "Medaritown",
+    description: "Medaritown adalah sebuah perusahaan perumahan yang berdedikasi untuk menghadirkan lingkungan hunian yang nyaman dan modern bagi masyarakat. Dengan fokus pada desain inovatif, keberlanjutan, dan kualitas bangunan, Medaritown telah membangun reputasi sebagai pengembang properti yang terkemuka. Perusahaan ini menawarkan berbagai tipe rumah mulai dari apartemen hingga rumah mewah, memberikan pilihan hunian yang sesuai dengan berbagai kebutuhan dan gaya hidup.",
+    fitur: [
+      {
+        name: "Landing Page",
+        description: "Landing Page yang menampilkan informasi tentang Medaritown dan layanan yang disediakan.",
+      },
+      {
+        name: "Call To Action",
+        description: "Call To Action yang memudahkan pengunjung untuk menghubungi Medaritown.",
+      },
+      {
+        name: "SEO",
+        description: "Memudahkan pencarian halaman web melalui mesin pencari.",
+      },
+      {
+        name: "Admin Panel",
+        description: "Admin Panel yang memudahkan pengelolaan konten dan informasi Medaritown.",
+      },
+      {
+        name: "Maps",
+        description: "Maps yang menampilkan lokasi Medaritown.",
+      }
+    ],
+    teams: [
+      {
+        name: "M. Adnan Zainul M. dan M. Reza Setiawan",
+        role: "Project Leader",
+      },
+      {
+        name: "Eker F. Arung, M. Zhafir Zaydan, dan Fakhriyannur Adha",
+        role: "UI/UX Designer",
+      },
+      {
+        name: "M. Ikhwan Pramudika",
+        role: "Front End",
+      },
+      {
+        name: "Joko Setyanto",
+        role: "Front End & Back End",
+      },
+      {
+        name: "Haikal Raditya F.",
+        role: "Back End",
+      },
+    ],
+    tech: [
+      "HTML",
+      "TailwindCSS",
+      "PHP Native",
+    ],
+    thumbnail: images["Medaritown.webp"],
+    url: null,
+    slug: "medaritown",
   }
 ];
 
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -8,7 +8,11 @@ const DetailPage = () => {
   const project = projectData.find((item) => item.slug === slug);
 
   if (!project) {
-    return <div className="p-6 text-center text-second text-5xl">Project not found</div>;
+    return (
+      <div className="p-6 text-center text-second text-5xl">
+        Project not found
+      </div>
+    );
   }
 
   return (
@@ -20,9 +24,51 @@ const DetailPage = () => {
         <FaBackspace />
         Kembali
       </button>
-      <img src={project.thumbnail} alt={project.title} className="rounded-lg mb-6" />
-      <h1 className="text-3xl font-bold text-center mb-4 text-second">{project.title}</h1>
-      <p className="text-lg text-center text-second">{project.description}</p>
+      <img
+        src={project.thumbnail}
+        alt={project.title}
+        className="rounded-lg md:rounded-3xl mb-6"
+      />
+      <div className="flex flex-col gap-4 mt-6">
+        <div className="mb-4 text-second flex flex-col gap-6">
+          <span className="font-bold text-3xl">🌐 {project.title}</span>
+          <span className="text-base md:text-lg text-justify ml-12">
+            {project.description}
+          </span>
+        </div>
+        <div className="mb-4 text-second flex flex-col gap-6">
+          <span className="font-bold text-3xl">🚀 Fitur Utama</span>
+          <ul className="list-disc list-inside text-base md:text-lg space-y-1">
+            {project.fitur.map((item, index) => (
+              <li key={index} className="ml-12">
+                {item.name} - {item.description}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="mb-4 text-second flex flex-col gap-6">
+          <span className="font-bold text-3xl">🐼 Anggota Team</span>
+          <ul className="list-disc list-inside text-base md:text-lg space-y-1">
+            {project.teams.map((member, index) => (
+              <li key={index} className="ml-12">
+                {member.name} as {member.role}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="mb-4 text-second flex flex-col gap-6">
+          <span className="font-bold text-3xl">
+            🛠️ Teknologi
+          </span>
+          <ul className="list-disc list-inside text-base md:text-lg space-y-1">
+            {project.tech.map((tech, index) => (
+              <li key={index} className="ml-12">
+                {tech}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
